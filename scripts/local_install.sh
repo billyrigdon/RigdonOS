0f6b2f0a69f197acca2cdfc60a0590251a55e75d
Billy Rigdon <billyrigdoniii@gmail.com>
2021-12-10 14:20:20 -0600

    Updated local build script

--- scripts/local_install.sh
+++ scripts/local_install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd ..
-rm -r ./backend/build;
-cd ./client;
+rm -r ./backend/client;
+cd ./frontend;
 npm install
 npm run build;
-mv ./dist ../backend/build;
-cd ..
+mv ./dist ../backend/client;
+cd ../backend
+npm install
+npm run build
 sudo mkdir /opt/RigdonOS
-sudo cp -r ./backend /opt/RigdonOS
-sudo cp ./scripts/RigdonOS.service /etc/systemd/system/RigdonOS.service
+sudo cp -r ./dist/* /opt/RigdonOS
+sudo cp ../scripts/RigdonOS.service /etc/systemd/system/RigdonOS.service
 sudo systemctl enable --now RigdonOS.service
