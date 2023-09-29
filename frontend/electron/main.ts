68b1c5d7b4dab4f4313ccc5afc85bbdbc7f9edce
Billy Rigdon <brigdon@rmsweb.com>
2023-09-29 06:15:08 -0500

    electron build

---
+++ frontend/electron/main.ts
@@ -0,0 +1,34 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow: BrowserWindow | null = null;
+
+const createWindow = (): void => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile('../index.html');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
